edfe39a7b9ee134f42ac1dbb8f68a95418d6bc61
Fernanda Rigolin <frzrigolin@gmail.com>
2022-12-09 21:05:56 -0300

    ajuste avatar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,7 @@ export function Sidebar(){
         src="https://images.unsplash.com/photo-1548092372-0d1bd40894a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <Avatar src= "https://media-exp1.licdn.com/dms/image/C4E03AQE6_JnND6STTA/profile-displayphoto-shrink_400_400/0/1653591822128?e=1662595200&v=beta&t=-AHYiP-oL2Tabk4jEtPqvVr5Uwe50XF3FlbNFE_qJgI" /> 
+        <Avatar src= "https://github.com/fernandazaccarorigolin.png" /> 
         <strong>Fernanda Rigolin</strong>
         <span>Web Developes</span>
       </div>
